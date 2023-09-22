afa638d38eb5f1b09f573dff15f858d4b4c70d3d
Ethan Whittaker <ethan.whittaker@students.snow.edu>
2023-09-22 16:16:39 -0600

    Added debouncing

--- hobby-manager/src/context/gameContext.tsx
+++ hobby-manager/src/context/gameContext.tsx
@@ -1,25 +1,46 @@
-import { useState, FC, ReactNode, createContext, useEffect } from 'react';
-import { IGame, listOfGames } from '../models/games';
+import { useState, FC, ReactNode, createContext, useEffect } from "react";
+import { IGame, listOfGames } from "../models/games";
 
 export interface GameContextType {
   games: IGame[];
   addGame: (g: IGame) => void;
-};
+}
 
 export const GameContext = createContext<GameContextType>({
   games: [],
   addGame: () => {},
 });
 
-const GameContextProvider: FC<{ children: ReactNode }> = ({
-  children,
-}) => {
+const storageKey = "storedGames";
+const storeGamesInLocalStorage = (games: IGame[]) => {
+  localStorage.setItem(storageKey, JSON.stringify(games));
+};
+
+const getGamesInLocalStorage = (): IGame[] => {
+  const stringifiedGames = localStorage.getItem(storageKey);
+  return JSON.parse(stringifiedGames || "[]");
+};
+
+const GameContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [gameIndex, setGameIndex] = useState<number>(3);
   const [games, setGames] = useState<IGame[]>([]);
 
   useEffect(() => {
-    setGames(listOfGames);
+    const gamesFromStorage = getGamesInLocalStorage();
+    if (gamesFromStorage.length === 0) {
+      setGames(listOfGames);
+    } else {
+      setGames(gamesFromStorage);
+    }
   }, []);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      storeGamesInLocalStorage(games);
+    }, 500);
+
+    return () => clearTimeout(handler);
+  }, [games]);
 
   const addGame = (g: IGame) => {
     const newGame: IGame = {
@@ -27,16 +48,16 @@ const GameContextProvider: FC<{ children: ReactNode }> = ({
       title: g.title,
       releaseYear: g.releaseYear,
       genre: g.genre,
-    }
+    };
     console.log(newGame);
-    setGameIndex(i => i + 1)
-    setGames([...games, newGame])
-  }
+    setGameIndex((i) => i + 1);
+    setGames([...games, newGame]);
+  };
 
   const startingValue: GameContextType = {
     games,
     addGame,
-  }
+  };
 
   return (
     <GameContext.Provider value={startingValue}>
